aa0a4e0f44903c219b934b814e5440acfcf0e6d9
Astriel Golondrina <88914067+Berlina24@users.noreply.github.com>
2025-07-24 17:31:37 +0800

    Update customize_smoothie.py
    added search on

--- customize_smoothie.py
+++ customize_smoothie.py
@@ -4,6 +4,7 @@ import streamlit as st
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 connection_parameters = {
    "account": "MKZROVY-VZB49003",
@@ -30,9 +31,12 @@ st.write("The name on your smoothie will be: ", name_on_order)
 # FETCH THE UPLOADED TABLE FROM STAGE ONLY FOCUSING ON THE FRUIT NAME COL
 # cnx = st.connection("snowflake")
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Select more than 5 fruits",
@@ -46,6 +50,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
