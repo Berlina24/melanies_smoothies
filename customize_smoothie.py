ec9714fd75fefbc2f72810e508e132bb3184a22f
Astriel Golondrina <88914067+Berlina24@users.noreply.github.com>
2025-07-24 16:22:25 +0800

    Update customize_smoothie.py
    Connected changes needed bc of move from SiS to SniS

--- customize_smoothie.py
+++ customize_smoothie.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 
@@ -10,6 +10,7 @@ st.write(
   """Orders that need to be filled!
   """
 )
+cnx = st.connection("snowflake")
 session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
