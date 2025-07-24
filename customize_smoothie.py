72cd018901147c04b270ac0ef22c2a84a4e25e74
Astriel Golondrina <88914067+Berlina24@users.noreply.github.com>
2025-07-24 16:37:14 +0800

    Update customize_smoothie.py

--- customize_smoothie.py
+++ customize_smoothie.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -16,6 +16,7 @@ st.write("The name on your smoothie will be: ", name_on_order)
 
 
 # FETCH THE UPLOADED TABLE FROM STAGE ONLY FOCUSING ON THE FRUIT NAME COL
+cnx = st.connection("snowflake")
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
